91480449caca047f5e275b40fb9a85b6d11be8f2
Manish <manishbabusujanimulk17@gmail.com>
2025-09-08 22:09:22 +0530

    project-structure

---
+++ src/test/java/com/lms/learningmanagementsystem/LearningmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.learningmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
